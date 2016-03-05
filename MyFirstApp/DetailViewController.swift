5991a86628c0d40eb2f15ef5261624cadcc14085
kucharz <jonaska94@gmail.com>
2016-03-05 13:08:41 +0100

    feat(detailView): add elements

--- MyFirstApp/DetailViewController.swift
+++ MyFirstApp/DetailViewController.swift
@@ -15,7 +15,11 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var forename: UILabel!
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var mail: UILabel!
+    @IBOutlet weak var phone: UILabel!
+    @IBOutlet weak var desk: UILabel!
+    @IBOutlet weak var webLink: UILabel!
 
+    @IBOutlet weak var address: UILabel!
     var detailItem: AnyObject? {
         didSet {
             // Update the view.
@@ -25,22 +29,35 @@ class DetailViewController: UIViewController {
 
     func configureView() {
         // Update the user interface for the detail item.
-        if let detail = self.detailItem as? Contact {
-            /*if let image = self.image {
-                let url = NSURL(string: detail.description)
+        if let name = self.name {
+            name.text = contact.name
+        }
+        if let forename = self.forename {
+            forename.text = contact.name
+        }
+        if let image = self.image {
+            Swift.print(contact.picture)
+            if contact.picture == "" {
+                image.image = UIImage(named: "Silhouette_Portrait")
+            }
+            else{
+                let url = NSURL(string: contact.picture)
                 let data = NSData(contentsOfURL: url!)
-                image.image = UIImage(data: data!)
-            }*/
-            if let name = self.name {
-                name.text = detail.name
+                let myImage = UIImage(data: data!)
+                image.image = myImage
             }
-            /*
-            if let email = mail {
-                let email = detail.mail
-                let url = NSURL(string: "mailto:\(email)")
-                UIApplication.sharedApplication().openURL(url)
-            }
-*/
+        }
+        if let email = self.mail {
+            email.text = contact.mail
+        }
+        if let phone = self.phone {
+            phone.text = contact.phone
+        }
+        if let desk = self.desk {
+            desk.text = contact.desk
+        }
+        if let webLink = self.webLink {
+            webLink.text = contact.website
         }
     }
 
--- MyFirstApp/MasterViewController.swift
+++ MyFirstApp/MasterViewController.swift
@@ -54,7 +54,7 @@ class MasterViewController: UITableViewController {
             if let indexPath = self.tableView.indexPathForSelectedRow {
                 let contact = objects[indexPath.row]
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = contact.name
+                controller.contact = contact
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
